c35477f46b33be164e97fcde207ab2a2d44ccc19
SoulGalaxy2312 <ngotri2312@gmail.com>
2024-11-11 21:06:42 +0700

    Test File Service, Approach to get FileInputPath

--- src/App.java
+++ src/App.java
@@ -1,5 +1,14 @@
+import services.FileService;
+
+import java.util.Map;
+
+import controllers.MainController;
+
+import java.util.List;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        MainController mainController = new MainController();
+        mainController.start();
     }
 }
